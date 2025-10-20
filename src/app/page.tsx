ce818b54f0555b403f1ac3ee7bcd4596fd40687a
André Wittwer <andre.wittwer@nav.no>
2025-10-20 14:44:09 +0200

    feat: decorator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import {Button} from "@navikt/ds-react";
-import {ThumbUpIcon} from "@navikt/aksel-icons";
-import {PageBlock} from "@navikt/ds-react/Page";
+import { Button, Heading, VStack } from '@navikt/ds-react'
+import { ThumbUpIcon } from '@navikt/aksel-icons'
+import { PageBlock } from '@navikt/ds-react/Page'
 
 export default function Home() {
-    return (
-        <main>
-            <PageBlock width="md" gutters>
-                <Button
-                    icon={<ThumbUpIcon title="a11y tittel" />}
-                    variant="primary"
-                >
-                    Gjør noe!
-                </Button>
-            </PageBlock>
-        </main>
-    );
+  return (
+    <VStack gap="space-12">
+      <Heading size={'large'} level="1" spacing>
+        Oppdater nærmeste leder
+      </Heading>
+      <PageBlock width="md" gutters>
+        <Button icon={<ThumbUpIcon title="a11y tittel" />} variant="primary">
+          Gjør noe!
+        </Button>
+      </PageBlock>
+    </VStack>
+  )
 }
