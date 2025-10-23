255eb023445c4eee71e9586a89c7bbc31c5fbb43
André Wittwer <andre.wittwer@nav.no>
2025-10-23 16:17:40 +0200

    chore: remove instrumentation

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-import { type Instrumentation } from 'next'
-import { logger } from '@navikt/next-logger'
-
-export const onRequestError: Instrumentation.onRequestError = async (error) => {
-  logger.error(error, 'Unexpected error on request')
-}
