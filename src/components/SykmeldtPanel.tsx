049ad457ee567fd4a18781bf640865258056408d
André Wittwer <andre.wittwer@nav.no>
2025-10-27 16:02:20 +0100

    chore: remove VStack

--- src/components/SykmeldtPanel.tsx
+++ src/components/SykmeldtPanel.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { NarmesteLederGetResponse } from '@/services/narmesteleder/narmestelederService'
-import { BodyShort, GuidePanel, Heading, Page, VStack } from '@navikt/ds-react'
+import { BodyLong, BodyShort, GuidePanel, Heading, Page, VStack } from '@navikt/ds-react'
 import { getFullName } from '@/services/narmesteleder/schemas/formSchema'
 
 export default function SykmeldtPanel({ sykmeldt }: { sykmeldt: NarmesteLederGetResponse }) {
@@ -13,24 +13,24 @@ export default function SykmeldtPanel({ sykmeldt }: { sykmeldt: NarmesteLederGet
       </Page.Block>
       <Page.Block width={'2xl'}>
         <GuidePanel>
-          <VStack gap="space-20">
-            <BodyShort>Hei xxx!</BodyShort>
-            <BodyShort>
-              <span className="font-bold">
-                {getFullName(sykmeldt.name)} ({sykmeldt.sykmeldtFnr})
-              </span>{' '}
-              er sykmeldt. Nav mangler informasjon om hvem som er nærmeste leder i bedrift{' '}
-              <span className="font-bold">{sykmeldt.orgnummer}</span>. Personen som oppgis som nærmeste leder, får
-              tilgang den sykmeldte ansatte og oppfølgingstjenestene Nav tilbyr på &quot;Dine sykmeldte&quot; hos Nav.
-            </BodyShort>
-            <BodyShort>
-              Vi har forhåndsutfylt skjemaet med navn og f.nr til den ansatte. Du må legge inn kontaktinformasjonen til
-              lederen som skal få tilgang.
-            </BodyShort>
-            <BodyShort>
-              Den ansatte vil se hvem bedriften har meldt inn som leder på &quot;Ditt sykefravær&quot; hos Nav
-            </BodyShort>
-          </VStack>
+          <Heading level="2" size="medium" spacing>
+            Hei xxx!
+          </Heading>
+          <BodyLong spacing>
+            <span className="font-bold">
+              {getFullName(sykmeldt.name)} ({sykmeldt.sykmeldtFnr})
+            </span>{' '}
+            er sykmeldt. Nav mangler informasjon om hvem som er nærmeste leder i bedrift{' '}
+            <span className="font-bold">{sykmeldt.orgnummer}</span>. Personen som oppgis som nærmeste leder, får tilgang
+            den sykmeldte ansatte og oppfølgingstjenestene Nav tilbyr på &quot;Dine sykmeldte&quot; hos Nav.
+          </BodyLong>
+          <BodyLong>
+            Vi har forhåndsutfylt skjemaet med navn og f.nr til den ansatte. Du må legge inn kontaktinformasjonen til
+            lederen som skal få tilgang.
+          </BodyLong>
+          <BodyLong>
+            Den ansatte vil se hvem bedriften har meldt inn som leder på &quot;Ditt sykefravær&quot; hos Nav
+          </BodyLong>
         </GuidePanel>
       </Page.Block>
     </Page>
