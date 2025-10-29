86e5c4a35e27eeebf60e23e7e62f34afc0244a89
André Wittwer <andre.wittwer@nav.no>
2025-10-29 13:54:08 +0100

    chore: remove unused code

--- src/components/AlertErrorNarmesteLeder.tsx
+++
@@ -1,8 +0,0 @@
-export function AlertErrorNarmesteLeder() {
-  return (
-    <div className="alert alert-error" role="alert">
-      <strong>Feil ved henting av nærmeste leder</strong>
-      <p>Det oppstod en feil ved henting av nærmeste leder. Vennligst prøv igjen senere.</p>
-    </div>
-  )
-}
