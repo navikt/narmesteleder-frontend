32c95c20d5c38c9f85358f5ba7dec42a08f7941f
André Wittwer <andre.wittwer@nav.no>
2025-10-30 17:31:46 +0100

    fix: order fields

--- src/components/form/LederGroup.tsx
+++ src/components/form/LederGroup.tsx
@@ -9,10 +9,10 @@ export const LederGroup = withFieldGroup({
         <group.AppField name="fodselsnummer">
           {(field) => <field.TextInputField label="Fødselsnummer (11 sifre)" />}
         </group.AppField>
-        <group.AppField name="mobilnummer">{(field) => <field.TextInputField label="Mobilnummer" />}</group.AppField>
-        <group.AppField name="epost">{(field) => <field.TextInputField label="E-post" />}</group.AppField>
         <group.AppField name="fornavn">{(field) => <field.TextInputField label="Fornavn" />}</group.AppField>
         <group.AppField name="etternavn">{(field) => <field.TextInputField label="Etternavn" />}</group.AppField>
+        <group.AppField name="epost">{(field) => <field.TextInputField label="E-post" />}</group.AppField>
+        <group.AppField name="mobilnummer">{(field) => <field.TextInputField label="Mobilnummer" />}</group.AppField>
       </>
     )
   },
