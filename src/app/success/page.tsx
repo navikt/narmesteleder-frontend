cb0a56b704cbf5206876c72b45e755beed51636a
André Wittwer <andre.wittwer@nav.no>
2025-11-06 07:39:33 +0100

    chore: vstack instead of div

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,18 +1,16 @@
 import ThankYouAlert from '@/components/form/ThankYouAlert'
 import { publicEnv } from '@/env-variables/publicEnv'
-import { Button } from '@navikt/ds-react'
+import { Button, VStack } from '@navikt/ds-react'
 import Link from 'next/link'
 
 export default function SuccessPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <VStack gap="6">
       <ThankYouAlert />
       
-      <div className="mt-6">
-        <Button as={Link} href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL} variant="secondary">
-          Min side arbeidsgiver
-        </Button>
-      </div>
-    </div>
+      <Button as={Link} href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL} variant="secondary">
+        Min side arbeidsgiver
+      </Button>
+    </VStack>
   )
 }
