ff0edf8d9159c25d0d13e7a61ead1414c3b88221
André Wittwer <andre.wittwer@nav.no>
2025-11-06 07:45:34 +0100

    chore: button design min side arbeidsgiver

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,16 +1,18 @@
 import ThankYouAlert from '@/components/form/ThankYouAlert'
 import { publicEnv } from '@/env-variables/publicEnv'
-import { Button, VStack } from '@navikt/ds-react'
+import { Button, HStack, VStack } from '@navikt/ds-react'
 import Link from 'next/link'
 
 export default function SuccessPage() {
   return (
     <VStack gap="6">
       <ThankYouAlert />
-      
-      <Button as={Link} href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL} variant="secondary">
-        Min side arbeidsgiver
-      </Button>
+      <HStack className="mt-0">
+        <Button as={Link} href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL} variant="secondary">
+          Min side arbeidsgiver
+        </Button>
+      </HStack>
+
     </VStack>
   )
 }
