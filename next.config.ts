4cbe6121f75648226014252ed85a830737e0a0cd
André Wittwer <andre.wittwer@nav.no>
2025-11-07 15:32:31 +0100

    chore: set turbopackFileSystemCacheForDev to true

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
-export const BASE_PATH = '/arbeidsgiver/ansatte/narmesteleder'
+export const BASE_PATH = "/arbeidsgiver/ansatte/narmesteleder";
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
+    optimizePackageImports: ["@navikt/ds-react", "@navikt/aksel-icons"],
+    turbopackFileSystemCacheForDev: true,
   },
   reactStrictMode: true,
   basePath: BASE_PATH,
-  output: 'standalone',
+  output: "standalone",
   productionBrowserSourceMaps: true,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
-}
+};
 
-export default nextConfig
+export default nextConfig;
