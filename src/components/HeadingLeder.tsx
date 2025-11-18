92c4108eca70456e7228eb29f38e8b5f98294fd7
André Wittwer <andre.wittwer@nav.no>
2025-11-18 10:22:18 +0100

    feat: heading leder

---
+++ src/components/HeadingLeder.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@navikt/ds-react";
+
+export function HeadingLeder() {
+  return (
+    <Heading size="large" level="1" spacing>
+      Oppgi nærmeste leder
+    </Heading>
+  );
+}
