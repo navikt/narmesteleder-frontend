f1ab56d5a0176620018499b00ea0722387ea9cae
André Wittwer <andre.wittwer@nav.no>
2025-11-18 13:23:13 +0100

    chore: typo in text

--- src/components/LederInfoDescription.tsx
+++ src/components/LederInfoDescription.tsx
@@ -1,7 +1,7 @@
 export function LederInfoDescription() {
   return (
     <p>
-      Personen som er oppgitt som nærmeste leder lar tilgang til den sykmeldte
+      Personen som er oppgitt som nærmeste leder får tilgang til den sykmeldte
       ansatte og oppfølgingstjenestene på &quot;Dine sykmeldte&quot; på Nav sine
       innloggede sider.
     </p>
