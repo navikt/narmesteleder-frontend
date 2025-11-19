eff51ee1d0d5c5e42c918bcfb5aec4bf73d43a51
André Wittwer <andre.wittwer@nav.no>
2025-11-19 16:20:54 +0100

    chore: more spacing for text

--- src/components/SykmeldtAndLederInfoPanel.tsx
+++ src/components/SykmeldtAndLederInfoPanel.tsx
@@ -1,14 +1,14 @@
-import { BodyShort, GuidePanel } from "@navikt/ds-react";
+import { BodyLong, BodyShort, GuidePanel } from "@navikt/ds-react";
 
 export default function SykmeldtAndLederInfoPanel() {
   return (
     <GuidePanel poster={true}>
-      <BodyShort spacing>
+      <BodyLong spacing>
         Her kan du oppgi hvem som er nærmeste leder for en sykmeldt ansatt.
         Dette er for å sikre at riktig leder skal få tilgang til
         oppfølgingstjenestene på &quot;Dine sykmeldte&quot; for å følge opp den
         ansatte som er syk.
-      </BodyShort>
+      </BodyLong>
 
       <BodyShort>
         Den ansatte må være sykmeldt for at det skal være mulig å oppgi inn hvem
