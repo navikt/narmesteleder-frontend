096d9be51940da6e304c9da3e24abb50747aeea2
André Wittwer <andre.wittwer@nav.no>
2025-11-19 08:23:08 +0100

    feat: withBackendMode function for either call the real or mock

---
+++ src/utils/withBackendMode.ts
@@ -0,0 +1,28 @@
+import { isLocalOrDemo } from "@/env-variables/envHelpers";
+
+type Fn<TArgs extends unknown[], TResult> = (
+  ...args: TArgs
+) => TResult | Promise<TResult>;
+
+type Implementation<TArgs extends unknown[], TResult> =
+  | Fn<TArgs, TResult>
+  | { fn: Fn<TArgs, TResult> };
+
+type BackendModeImpls<TArgs extends unknown[], TResult> = {
+  real: Implementation<TArgs, TResult>;
+  fake: Implementation<TArgs, TResult>;
+};
+
+export const withBackendMode = <TArgs extends unknown[], TResult>(
+  impls: BackendModeImpls<TArgs, TResult>,
+): Fn<TArgs, TResult> => {
+  return (...args: TArgs) => {
+    const impl = isLocalOrDemo ? impls.fake : impls.real;
+    if (typeof impl === "function") {
+      return impl(...args);
+    }
+    throw new Error(
+      "Provided implementation does not contain a valid function.",
+    );
+  };
+};
---
+++ tests/utils/withBackendMode.test.ts
@@ -0,0 +1,41 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import * as envHelpers from "@/env-variables/envHelpers";
+import { withBackendMode } from "@/utils/withBackendMode";
+
+describe("withBackendMode", () => {
+  const realFn = vi.fn((x: number) => x + 1);
+  const fakeFn = vi.fn((x: number) => x - 1);
+
+  beforeEach(() => {
+    realFn.mockClear();
+    fakeFn.mockClear();
+  });
+
+  it("calls real implementation when isLocalOrDemo is false", () => {
+    vi.spyOn(envHelpers, "isLocalOrDemo", "get").mockReturnValue(false);
+
+    const fn = withBackendMode({ real: realFn, fake: fakeFn });
+    expect(fn(5)).toBe(6);
+    expect(realFn).toHaveBeenCalledWith(5);
+    expect(fakeFn).not.toHaveBeenCalled();
+  });
+
+  it("calls fake implementation when isLocalOrDemo is true", () => {
+    vi.spyOn(envHelpers, "isLocalOrDemo", "get").mockReturnValue(true);
+
+    const fn = withBackendMode({ real: realFn, fake: fakeFn });
+    expect(fn(5)).toBe(4);
+    expect(fakeFn).toHaveBeenCalledWith(5);
+    expect(realFn).not.toHaveBeenCalled();
+  });
+
+  it("throws error if object impl does not contain a function", () => {
+    vi.spyOn(envHelpers, "isLocalOrDemo", "get").mockReturnValue(true);
+
+    // @ts-expect-error purposely wrong for test
+    const fn = withBackendMode({ real: realFn, fake: { fn: null } });
+    expect(() => fn(5)).toThrow(
+      "Provided implementation does not contain a valid function.",
+    );
+  });
+});
