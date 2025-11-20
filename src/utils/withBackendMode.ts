84957be3f58a1c2289708d05f8ff941993d252c4
André Wittwer <andre.wittwer@nav.no>
2025-11-20 14:36:48 +0100

    chore: fix async and make IntelliJ happy

--- src/utils/withBackendMode.ts
+++ src/utils/withBackendMode.ts
@@ -2,23 +2,20 @@ import { isLocalOrDemo } from "@/env-variables/envHelpers";
 
 type Fn<TArgs extends unknown[], TResult> = (
   ...args: TArgs
-) => TResult | Promise<TResult>;
-
-type Implementation<TArgs extends unknown[], TResult> =
-  | Fn<TArgs, TResult>
-  | { fn: Fn<TArgs, TResult> };
+) => Promise<TResult>;
 
 type BackendModeImpls<TArgs extends unknown[], TResult> = {
-  real: Implementation<TArgs, TResult>;
-  fake: Implementation<TArgs, TResult>;
+  real: Fn<TArgs, TResult>;
+  fake: Fn<TArgs, TResult>;
 };
 
 export const withBackendMode = <TArgs extends unknown[], TResult>(
   impls: BackendModeImpls<TArgs, TResult>,
 ): Fn<TArgs, TResult> => {
   const impl = isLocalOrDemo ? impls.fake : impls.real;
+
   if (typeof impl === "function") {
-    return impl;
+    return async (...args: TArgs) => impl(...args);
   }
   throw new Error("Provided implementation does not contain a valid function.");
 };
--- tests/utils/withBackendMode.test.ts
+++ tests/utils/withBackendMode.test.ts
@@ -3,28 +3,28 @@ import * as envHelpers from "@/env-variables/envHelpers";
 import { withBackendMode } from "@/utils/withBackendMode";
 
 describe("withBackendMode", () => {
-  const realFn = vi.fn((x: number) => x + 1);
-  const fakeFn = vi.fn((x: number) => x - 1);
+  const realFn = vi.fn(async (x: number) => x + 1);
+  const fakeFn = vi.fn(async (x: number) => x - 1);
 
   beforeEach(() => {
     realFn.mockClear();
     fakeFn.mockClear();
   });
 
-  it("calls real implementation when isLocalOrDemo is false", () => {
+  it("calls real implementation when isLocalOrDemo is false", async () => {
     vi.spyOn(envHelpers, "isLocalOrDemo", "get").mockReturnValue(false);
 
     const fn = withBackendMode({ real: realFn, fake: fakeFn });
-    expect(fn(5)).toBe(6);
+    expect(await fn(5)).toBe(6);
     expect(realFn).toHaveBeenCalledWith(5);
     expect(fakeFn).not.toHaveBeenCalled();
   });
 
-  it("calls fake implementation when isLocalOrDemo is true", () => {
+  it("calls fake implementation when isLocalOrDemo is true", async () => {
     vi.spyOn(envHelpers, "isLocalOrDemo", "get").mockReturnValue(true);
 
     const fn = withBackendMode({ real: realFn, fake: fakeFn });
-    expect(fn(5)).toBe(4);
+    expect(await fn(5)).toBe(4);
     expect(fakeFn).toHaveBeenCalledWith(5);
     expect(realFn).not.toHaveBeenCalled();
   });
