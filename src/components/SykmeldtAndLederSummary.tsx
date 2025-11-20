68d033e41f49ae130ec4ff0da05b88e091e8034f
André Wittwer <andre.wittwer@nav.no>
2025-11-20 08:16:42 +0100

    feat: new design summary sickleave and leader form

--- src/components/SykmeldtAndLederSummary.tsx
+++ src/components/SykmeldtAndLederSummary.tsx
@@ -6,42 +6,63 @@ export function SykmeldtAndLederSummary() {
   const { submittedData, handleEdit } = useSykmeldtAndLederContextState();
 
   return (
-    <FormSummary>
-      <FormSummary.Header>
-        <VStack gap="2">
-          <p>Nærmeste leder for sykmeldt person med fødselsnummer</p>
-          <FormSummary.Heading level="3">
-            {formatFnr(submittedData.sykmeldt.fodselsnummer)}
-          </FormSummary.Heading>
-        </VStack>
-      </FormSummary.Header>
+    <VStack gap="8">
+      <FormSummary>
+        <FormSummary.Header>
+          <FormSummary.Heading level="3">Sykmeldt</FormSummary.Heading>
+        </FormSummary.Header>
 
-      <FormSummary.Answers>
-        <FormSummary.Answer>
-          <FormSummary.Label>
-            Fødselsnummer til nærmeste leder
-          </FormSummary.Label>
-          <FormSummary.Value>
-            {formatFnr(submittedData.leder.fodselsnummer)}
-          </FormSummary.Value>
-        </FormSummary.Answer>
-        <FormSummary.Answer>
-          <FormSummary.Label>Epost</FormSummary.Label>
-          <FormSummary.Value>{submittedData.leder.epost}</FormSummary.Value>
-        </FormSummary.Answer>
-        <FormSummary.Answer>
-          <FormSummary.Label>Mobilnummer</FormSummary.Label>
-          <FormSummary.Value>
-            {submittedData.leder.mobilnummer}
-          </FormSummary.Value>
-        </FormSummary.Answer>
-      </FormSummary.Answers>
-      <FormSummary.Footer>
-        <FormSummary.EditLink
-          onClick={handleEdit}
-          style={{ cursor: "pointer" }}
-        />
-      </FormSummary.Footer>
-    </FormSummary>
+        <FormSummary.Answers>
+          <FormSummary.Answer>
+            <FormSummary.Label>Fødselsnummer</FormSummary.Label>
+            <FormSummary.Value>
+              {formatFnr(submittedData.sykmeldt.fodselsnummer)}
+            </FormSummary.Value>
+          </FormSummary.Answer>
+          <FormSummary.Answer>
+            <FormSummary.Label>Organisasjonsnummer</FormSummary.Label>
+            <FormSummary.Value>
+              {submittedData.sykmeldt.orgnummer}
+            </FormSummary.Value>
+          </FormSummary.Answer>
+        </FormSummary.Answers>
+        <FormSummary.Footer>
+          <FormSummary.EditLink
+            onClick={handleEdit}
+            style={{ cursor: "pointer" }}
+          />
+        </FormSummary.Footer>
+      </FormSummary>
+      <FormSummary>
+        <FormSummary.Header>
+          <FormSummary.Heading level="3">Nærmeste leder</FormSummary.Heading>
+        </FormSummary.Header>
+
+        <FormSummary.Answers>
+          <FormSummary.Answer>
+            <FormSummary.Label>Fødselsnummer</FormSummary.Label>
+            <FormSummary.Value>
+              {formatFnr(submittedData.leder.fodselsnummer)}
+            </FormSummary.Value>
+          </FormSummary.Answer>
+          <FormSummary.Answer>
+            <FormSummary.Label>Epost</FormSummary.Label>
+            <FormSummary.Value>{submittedData.leder.epost}</FormSummary.Value>
+          </FormSummary.Answer>
+          <FormSummary.Answer>
+            <FormSummary.Label>Mobilnummer</FormSummary.Label>
+            <FormSummary.Value>
+              {submittedData.leder.mobilnummer}
+            </FormSummary.Value>
+          </FormSummary.Answer>
+        </FormSummary.Answers>
+        <FormSummary.Footer>
+          <FormSummary.EditLink
+            onClick={handleEdit}
+            style={{ cursor: "pointer" }}
+          />
+        </FormSummary.Footer>
+      </FormSummary>
+    </VStack>
   );
 }
