5bd00ab2e26e97746c5165bc185617ed006d2898
André Wittwer <andre.wittwer@nav.no>
2025-12-01 13:01:25 +0100

    fix: links in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BugIcon } from "@navikt/aksel-icons";
 import {
   BodyShort,
@@ -11,12 +10,13 @@ import {
 } from "@navikt/ds-react";
 import { ListItem } from "@navikt/ds-react/List";
 import { PageBlock } from "@navikt/ds-react/Page";
+import { publicEnv } from "@/env-variables/publicEnv";
 
 const NotFound = async () => (
   <PageBlock as="main" width="xl" gutters>
     <BoxNew paddingBlock="space-80 space-64" data-aksel-template="404-v2">
       <VStack gap="space-64">
-        <VStack gap="space-48" align="start">
+        <VStack gap="space-24" align="start">
           <div>
             <Heading level="1" size="large" spacing>
               Beklager, vi fant ikke siden
@@ -25,32 +25,29 @@ const NotFound = async () => (
               Denne siden kan være slettet eller flyttet, eller det er en feil i
               lenken.
             </BodyShort>
-            <List>
-              <ListItem>Bruk gjerne søket eller menyen</ListItem>
-              <ListItem>
-                <Link href="#">Gå til forsiden</Link>
-              </ListItem>
-            </List>
           </div>
-          <Button as="a" href="#">
-            Gå til Min side
+          <Button
+            as="a"
+            href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL}
+            variant="primary"
+          >
+            Gå til Min side arbeidsgiver
           </Button>
-          <Link href="#">
-            <BugIcon aria-hidden />
-            Meld gjerne fra om at lenken ikke virker
-          </Link>
         </VStack>
 
-        <div>
-          <Heading level="2" size="large" spacing>
+        <VStack gap="space-24" align="start">
+          <Heading level="2" size="large">
             Page not found
           </Heading>
           <BodyShort spacing>The page you requested cannot be found.</BodyShort>
-          <BodyShort>
-            Go to the <Link href="#">front page</Link>, or use one of the links
-            in the menu.
-          </BodyShort>
-        </div>
+          <Button
+            as="a"
+            href={publicEnv.NEXT_PUBLIC_MIN_SIDE_ARBEIDSGIVER_URL}
+            variant="primary"
+          >
+            Go to page Min side arbeidsgiver
+          </Button>
+        </VStack>
       </VStack>
     </BoxNew>
   </PageBlock>
