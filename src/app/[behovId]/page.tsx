fd3c395d4dc9d6df93051bafcdba27dd808c6f0b
André Wittwer <andre.wittwer@nav.no>
2025-12-05 10:15:15 +0100

    chore: try fixing error handling

--- src/app/[behovId]/page.tsx
+++ src/app/[behovId]/page.tsx
@@ -1,12 +1,57 @@
 import { Suspense } from "react";
 import { logger } from "@navikt/next-logger";
 import notFound from "@/app/not-found";
-import { LederInfoLoader } from "@/components/LederInfoLoader";
+import ErrorAlert from "@/components/ErrorAlert";
 import { LederInfoSpinner } from "@/components/LederInfoSpinner";
 import { requirementIdSchema } from "@/schemas/requirementSchema";
+import { LederInfo, fetchLederInfo } from "@/server/fetchData/fetchLederInfo";
+import type { ErrorDetail } from "@/server/narmesteLederErrors";
+import { LederViewControl } from "../../components/LederViewControl";
 
 const isValidBehovId = (behovId: string) =>
   !requirementIdSchema.safeParse(behovId).success;
+
+const LederInfoContent = async ({ behovId }: { behovId: string }) => {
+  let lederInfo: LederInfo | null = null;
+  let errorDetail: ErrorDetail | undefined;
+
+  try {
+    logger.info(`Fetching lederInfo for behovId=${behovId}`);
+    lederInfo = await fetchLederInfo(behovId);
+  } catch (error) {
+    logger.error(`Error fetching lederInfo for behovId=${behovId}: ${error}`);
+    // if (isFrontendError(error)) {
+    //   logger.info(`FrontendError while fetching leder info for behovId=${behovId}: ${error.message}`);
+    //   errorDetail = error.errorDetail;
+    // } else {
+    errorDetail = {
+      title: "Noe gikk galt",
+      message:
+        "Det oppstod en uventet feil ved henting av informasjon om nærmeste leder. Vennligst prøv igjen senere.",
+      // };
+    };
+    logger.error(
+      `ErrorDetail for behovId=${behovId}: ${errorDetail.title} - ${errorDetail.message}`,
+    );
+  }
+
+  logger.info("here i am");
+  if (errorDetail) {
+    logger.error(
+      `Error fetching lederInfo for behovId=${behovId}: ${errorDetail.title} - ${errorDetail.message}`,
+    );
+
+    return <ErrorAlert detail={errorDetail} />;
+  }
+
+  if (!lederInfo) {
+    logger.error(`No lederInfo found for behovId=${behovId}`);
+
+    return notFound();
+  }
+
+  return <LederViewControl lederInfo={lederInfo} behovId={behovId} />;
+};
 
 export default async function Home({
   params,
@@ -21,7 +66,7 @@ export default async function Home({
 
   return (
     <Suspense fallback={<LederInfoSpinner />}>
-      <LederInfoLoader behovId={behovId} />
+      <LederInfoContent behovId={behovId} />
     </Suspense>
   );
 }
