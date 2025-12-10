4dd41116d78151d3423b1ffaa10f6df700a57212
André Wittwer <andre.wittwer@nav.no>
2025-12-10 13:21:54 +0100

    chore: refactor to own component, pr feedback

--- src/app/[behovId]/page.tsx
+++ src/app/[behovId]/page.tsx
@@ -1,35 +1,12 @@
 import { Suspense } from "react";
 import { logger } from "@navikt/next-logger";
 import notFound from "@/app/not-found";
-import LederInfoError from "@/components/LederInfoError";
+import { LederInfoLoader } from "@/components/LederInfoLoader";
 import { LederInfoSpinner } from "@/components/LederInfoSpinner";
 import { requirementIdSchema } from "@/schemas/requirementSchema";
-import { LederInfo, fetchLederInfo } from "@/server/fetchData/fetchLederInfo";
-import { isFrontendError } from "@/server/narmesteLederErrorUtils";
-import { LederViewControl } from "../../components/LederViewControl";
 
 const isValidBehovId = (behovId: string) =>
   !requirementIdSchema.safeParse(behovId).success;
-
-const LederInfoContent = async ({ behovId }: { behovId: string }) => {
-  let lederInfo: LederInfo | null = null;
-
-  try {
-    lederInfo = await fetchLederInfo(behovId);
-  } catch (error) {
-    if (isFrontendError(error)) {
-      return <LederInfoError detail={error.errorDetail} />;
-    } else {
-      throw error;
-    }
-  }
-
-  if (!lederInfo) {
-    return notFound();
-  }
-
-  return <LederViewControl lederInfo={lederInfo} behovId={behovId} />;
-};
 
 export default async function Home({
   params,
@@ -44,7 +21,7 @@ export default async function Home({
 
   return (
     <Suspense fallback={<LederInfoSpinner />}>
-      <LederInfoContent behovId={behovId} />
+      <LederInfoLoader behovId={behovId} />
     </Suspense>
   );
 }
