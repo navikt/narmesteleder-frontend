becce0de79169a63293d61e654625da2aabd6c52
André Wittwer <andre.wittwer@nav.no>
2025-12-11 11:46:48 +0100

    fix: better default config for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: 2,
+  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
     baseURL: "http://localhost:3000/arbeidsgiver/ansatte/narmesteleder/",
-    trace: "retry-with-trace",
+    trace: "on-first-retry",
   },
   projects: [
     {
