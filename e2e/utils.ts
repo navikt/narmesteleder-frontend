d50a9db026de193302240ea04d039cd547c6de6a
André Wittwer <andre.wittwer@nav.no>
2025-12-12 08:39:37 +0100

    test: add Playwright e2e tests

---
+++ e2e/utils.ts
@@ -0,0 +1,30 @@
+import { Page, expect } from "@playwright/test";
+import { TestId } from "@/utils/testIds";
+import { ValidationMessages } from "@/utils/validationMessages";
+
+const getByText = (page: Page, validationMessage: ValidationMessages) =>
+  page.getByText(validationMessage);
+
+export const getByTestId = (page: Page, testId: TestId) =>
+  page.getByTestId(testId);
+
+export const expectAllVisible = async (page: Page, testIds: TestId[]) => {
+  for (const testId of testIds) {
+    await expect(getByTestId(page, testId)).toBeVisible();
+  }
+};
+
+export const expectAllCount = async (
+  page: Page,
+  items: Array<[ValidationMessages, number]>,
+) => {
+  for (const [message, count] of items) {
+    await expect(getByText(page, message)).toHaveCount(count);
+  }
+};
+
+export const fillAll = async (page: Page, fields: Array<[TestId, string]>) => {
+  for (const [testId, value] of fields) {
+    await getByTestId(page, testId).fill(value);
+  }
+};
