7c899acd2b62f0a39a8ee5711b0314a9e531ff23
André Wittwer <andre.wittwer@nav.no>
2026-01-13 16:32:22 +0100

    chore: add CSP header configuration in next config (#173)
    * chore: add csp header in next config
    
    * fix: warning reparsing error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,39 @@
 import type { NextConfig } from "next";
 
-export const BASE_PATH = "/arbeidsgiver/ansatte/narmesteleder";
+const environment =
+  process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "prod" ? "prod" : "dev";
+
+const SELF = "'self'";
+
+const appDirectives = {
+  "default-src": [SELF],
+  "script-src": [SELF, "'unsafe-eval'"],
+  "script-src-elem": [SELF],
+  "style-src": [SELF],
+  "style-src-elem": [SELF],
+  "img-src": [SELF, "data:"],
+  "font-src": [SELF, "https://cdn.nav.no"],
+  "worker-src": [SELF],
+  "connect-src": [SELF, "https://*.nav.no"],
+};
 
 const nextConfig: NextConfig = {
+  async headers() {
+    const { buildCspHeader } =
+      await import("@navikt/nav-dekoratoren-moduler/ssr");
+    const cspValue = await buildCspHeader(appDirectives, { env: environment });
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspValue,
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
   experimental: {
     optimizePackageImports: ["@navikt/ds-react", "@navikt/aksel-icons"],
@@ -10,7 +41,7 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: true,
   reactCompiler: true,
-  basePath: BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   output: "standalone",
   productionBrowserSourceMaps: true,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,10 +3,10 @@
 import { PropsWithChildren } from "react";
 import { Theme } from "@navikt/ds-react/Theme";
 import { configureLogger } from "@navikt/next-logger";
-import { BASE_PATH } from "../../next.config";
+import { publicEnv } from "@/env-variables/publicEnv";
 
 configureLogger({
-  basePath: BASE_PATH,
+  basePath: publicEnv.NEXT_PUBLIC_BASE_PATH,
 });
 
 export default function Providers({ children }: PropsWithChildren) {
