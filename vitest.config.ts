505a07350b7a39e9947fd507b2f6b446c47f04bb
André Wittwer <andre.wittwer@nav.no>
2026-04-07 10:50:05 +0200

    chore(deps): make vite updates explicit for Dependabot

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     setupFiles: "./vitest.setup.ts",
     exclude: ["**/node_modules/**", "**/e2e/**", ".next/**"],
