79aa07c8fe0ed72f54e35f067a0a784f4c291f27
malazkoj <malaz.alkoj@nav.no>
2026-05-19 13:27:24 +0200

    Fix for build i dev.

--- src/app/(registrering)/page.tsx
+++ src/app/(registrering)/page.tsx
@@ -1,6 +1,8 @@
 import { ViewControl } from "@/app/(registrering)/components/ViewControl";
 import { fetchOrganisasjoner } from "@/server/fetchData/fetchOrganisasjoner";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const organisasjonerResult = await fetchOrganisasjoner();
 
